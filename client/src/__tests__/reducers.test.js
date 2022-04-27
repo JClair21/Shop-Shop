14947debb5bdeb3d44801819d9946a86747faab9
JClair21 <jessicagoodling@gmail.com>
2022-04-27 14:41:31 -0400

    correcting code

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -1,3 +1,7 @@
+import {
+    reducer
+} from '../utils/reducers';
+
 //import our actions
 import {
     UPDATE_PRODUCTS,
@@ -5,25 +9,38 @@ import {
     UPDATE_CURRENT_CATEGORY
 } from '../utils/actions';
 
-//import our reducer
-import { reducer } from '../utils/reducers';
 
 //create a sample of what our global state will look like
 const initialState = {
     products: [],
-    categories: [{ name: 'Food' }],
+    categories: [{
+        name: 'Food'
+    }],
     currentCategory: '1',
 };
 
-Test('UPDATE_PRODUCTS', () => {
+test('UPDATE_PRODUCTS', () => {
     let newState = reducer(initialState, {
         type: UPDATE_PRODUCTS,
         products: [{}, {}]
     });
-    })
+})
 
-    test('UPDATE_PRODUCTS', () => {
-        let newState = reducer(initialState, {
-            type: UPDATE_PRODUCTS,
-        });
-    })
+expect(newState.categories.length).toBe(2);
+expect(initialState.categories.length).toBe(1);
+
+test('UPDATE_PRODUCTS', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_PRODUCTS,
+    });
+});
+
+test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES,
+        categories: [{}, {}]
+    });
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+});
